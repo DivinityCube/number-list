916c6c44c9e614a4ede424feaf2b811307d03221
Taysy <104109140+DivinityCube@users.noreply.github.com>
2024-09-26 15:19:42 +0100

    0.70-1: Fix erroneous version label in the About window

--- main.py
+++ main.py
@@ -600,7 +600,7 @@ def about(window):
   title_label.pack()
   update_label = tk.Label(about_window, text="The 'Transformative' Update")
   update_label.pack()
-  version_label = tk.Label(about_window, text="Version 0.70 BETA 2")
+  version_label = tk.Label(about_window, text="Version 0.70-1")
   version_label.pack()
   contributor_label = tk.Label(about_window, text="Contributors:")
   contributor_label.pack()
